64e6ea8a69143e0ae6e8b833c0b42cd3bf5153d7
Willian Fernando Proni <5by5.proni@voeazul.com.br>
2023-06-14 11:33:37 -0300

    Aula 2.3 - UPDATE - Lidando com IDs, UUIDs e granularidade de updates de uma entidade

--- core/crud.ts
+++ core/crud.ts
@@ -1,13 +1,15 @@
 
 import fs from "fs";
 import { ResultTodos, Todo } from "./models";
+import { v4 as uuid } from 'uuid';
 
 const DB_FILE_PATH = './core/db' //caminho do arquivo
 console.log('[CRUD]')
 
 //writeFileSync --> escrever um arquivo de maneira assíncrona fs.writeFileSync('caminho do arquivo (onde vai ser salvo)', conteúdo do arquivo)
-function create(content: string){ //Simula a criação de um dado no banco, aqui estamos criando um arquivo com o dado enviado
+function create(content: string): Todo{ //Simula a criação de um dado no banco, aqui estamos criando um arquivo com o dado enviado
     const todo : Todo = { //Criando um objeto JSON
+        id: uuid(),
         date: new Date().toISOString(),
         content: content,
         done: false
@@ -22,7 +24,7 @@ function create(content: string){ //Simula a criação de um dado no banco, aqui
     fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
         todos,
     }, null, 2)) //salvando o conteúdo(content) no arquivo DB_FILE_PATH
-    return content;
+    return todo;
 }
 
  function read(): Todo[]{
@@ -32,6 +34,30 @@ function create(content: string){ //Simula a criação de um dado no banco, aqui
     return db.todos
 }
 
+function update(uuid: string, contentUpdate: Partial<Todo>) {
+   let updateTodo;
+   const todos = read();
+   const hasResultFindId =  todos.find(({id}) => id === uuid);
+
+   if(hasResultFindId){
+        updateTodo = Object.assign(hasResultFindId, contentUpdate);
+        fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos}, null, 2));
+    }
+
+    if(!updateTodo){
+        throw new Error("Please, provide another ID!");
+    }
+
+    return updateTodo
+}
+
+function updateContentById(id: string, content: string){
+    update(id, {
+        content
+    })
+
+}
+
 function CLEAR_DB() {
     fs.writeFileSync(DB_FILE_PATH, '')
 }
@@ -39,5 +65,6 @@ function CLEAR_DB() {
 //simulação
 CLEAR_DB()
 create('Primeira TODO')
-create('Segunda TODO')
+const responseTwoCreate = create('Segunda TODO')
+updateContentById(responseTwoCreate.id, 'Atualização do content')
 console.log(read())
--- core/models.ts
+++ core/models.ts
@@ -3,6 +3,7 @@ export interface ResultTodos {
 }
 
 export interface Todo {
+    id: string;
     date: string;
     content: string;
     done: boolean;
